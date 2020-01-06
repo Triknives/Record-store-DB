d7b2c835c644ba74a17daade0c7163bbe77ddae5
Student <example@example.com>
2020-01-06 10:49:44 -0800

    update song_spec and song/album.rb. add before:each

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ get ('/albums/:id') do
   @album = Album.find(params[:id].to_i())
   erb(:album)
 end
-#
+
 get ('/albums/:id/edit') do
   @album = Album.find(params[:id].to_i())
   erb(:edit_album)
